8d3467d2a86f547e843f2ccdbda01900556495c9
TMRGZ <miguelrg5605@outlook.es>
2023-01-28 14:52:33 +0100

    FEATURE: Added microservices implementation

--- Safebox-Auth/src/main/java/com/rviewer/skeletons/SafeboxAuthApplication.java
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/SafeboxAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RviewerSkeletonApplication {
+public class SafeboxAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RviewerSkeletonApplication.class, args);
+		SpringApplication.run(SafeboxAuthApplication.class, args);
 	}
 
 }
---
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/SafeboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rviewer.skeletons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeboxServiceApplication.class, args);
+	}
+
+}
