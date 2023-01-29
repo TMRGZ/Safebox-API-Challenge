36204f045dcdeeac259a200f36d4024dc020b3d8
TMRGZ <miguelrg5605@outlook.es>
2023-01-29 17:23:30 +0100

    FEATURE: Added converters to mappers for handling nested objects

--- Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/mapper/dao/SafeboxDaoMapper.java
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/mapper/dao/SafeboxDaoMapper.java
@@ -1,11 +1,17 @@
 package com.rviewer.skeletons.infrastructure.mapper.dao;
 
+import com.rviewer.skeletons.domain.model.Item;
 import com.rviewer.skeletons.domain.model.Safebox;
+import com.rviewer.skeletons.infrastructure.persistence.dao.ItemDao;
 import com.rviewer.skeletons.infrastructure.persistence.dao.SafeboxDao;
+import org.modelmapper.Conditions;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +20,18 @@ public class SafeboxDaoMapper {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private ItemDaoMapper itemDaoMapper;
+
+    @PostConstruct
+    public void setup() {
+        TypeMap<SafeboxDao, Safebox> propertyMapper = modelMapper.createTypeMap(SafeboxDao.class, Safebox.class);
+        Converter<List<ItemDao>, List<Item>> daoToDomain = dao -> itemDaoMapper.map(dao.getSource());
+        propertyMapper.addMappings(
+                mapper -> mapper.when(Conditions.isNotNull()).using(daoToDomain).map(SafeboxDao::getItemList, Safebox::setItemList)
+        );
+    }
 
     public Safebox map(SafeboxDao safeboxDao) {
         return modelMapper.map(safeboxDao, Safebox.class);
--- Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/mapper/domain/SafeboxMapper.java
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/mapper/domain/SafeboxMapper.java
@@ -1,11 +1,17 @@
 package com.rviewer.skeletons.infrastructure.mapper.domain;
 
+import com.rviewer.skeletons.domain.model.Item;
 import com.rviewer.skeletons.domain.model.Safebox;
+import com.rviewer.skeletons.infrastructure.persistence.dao.ItemDao;
 import com.rviewer.skeletons.infrastructure.persistence.dao.SafeboxDao;
+import org.modelmapper.Conditions;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +20,18 @@ public class SafeboxMapper {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @PostConstruct
+    public void setup() {
+        TypeMap<Safebox, SafeboxDao> propertyMapper = modelMapper.createTypeMap(Safebox.class, SafeboxDao.class);
+        Converter<List<Item>, List<ItemDao>> domainToDao = domain -> itemMapper.map(domain.getSource());
+        propertyMapper.addMappings(
+                mapper -> mapper.when(Conditions.isNotNull()).using(domainToDao).map(Safebox::getItemList, SafeboxDao::setItemList)
+        );
+    }
 
     public SafeboxDao map(Safebox safebox) {
         return modelMapper.map(safebox, SafeboxDao.class);
