8d05a77a151ddea569a84abe9cb6f0938a42e1d9
TMRGZ <miguelrg5605@outlook.es>
2023-01-29 14:25:23 +0100

    FEATURE: Added repository implementation

---
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/persistence/repository/impl/SafeboxRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.rviewer.skeletons.infrastructure.persistence.repository.impl;
+
+import com.rviewer.skeletons.domain.model.Safebox;
+import com.rviewer.skeletons.domain.repository.SafeboxRepository;
+import com.rviewer.skeletons.infrastructure.mapper.dao.SafeboxDaoMapper;
+import com.rviewer.skeletons.infrastructure.mapper.domain.SafeboxMapper;
+import com.rviewer.skeletons.infrastructure.persistence.repository.JpaSafeboxRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class SafeboxRepositoryImpl implements SafeboxRepository {
+
+    @Autowired
+    private JpaSafeboxRepository safeboxRepository;
+
+    @Autowired
+    private SafeboxMapper safeboxMapper;
+
+    @Autowired
+    private SafeboxDaoMapper safeboxDaoMapper;
+
+    @Override
+    public Optional<Safebox> findById(Long id) {
+        return safeboxRepository.findById(id).map(safeboxDao -> safeboxDaoMapper.map(safeboxDao));
+    }
+
+    @Override
+    public Optional<Safebox> findByOwner(String owner) {
+        return safeboxRepository.findByOwner(owner).map(safeboxDao -> safeboxDaoMapper.map(safeboxDao));
+    }
+
+    @Override
+    public Safebox save(Safebox safebox) {
+        return safeboxDaoMapper.map(safeboxRepository.save(safeboxMapper.map(safebox)));
+    }
+}
