e1b063d268f7fc0ecbf3990e0227e45be7b2ba07
TMRGZ <miguelrg5605@outlook.es>
2023-01-30 21:43:29 +0100

    FEATURE: Optimized imports

--- Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/persistence/dao/ItemDao.java
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/persistence/dao/ItemDao.java
@@ -3,7 +3,10 @@ package com.rviewer.skeletons.infrastructure.persistence.dao;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Getter
