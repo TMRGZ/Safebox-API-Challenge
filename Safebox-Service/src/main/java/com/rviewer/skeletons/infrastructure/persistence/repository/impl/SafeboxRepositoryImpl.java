5b830c88f6090c3b3b4105ce859710c31affd509
TMRGZ <miguelrg5605@outlook.es>
2023-02-02 23:20:44 +0100

    SONAR: Optimized imports

--- Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/persistence/repository/impl/SafeboxRepositoryImpl.java
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/infrastructure/persistence/repository/impl/SafeboxRepositoryImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Component
 public class SafeboxRepositoryImpl implements SafeboxRepository {
