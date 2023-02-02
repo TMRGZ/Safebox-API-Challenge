0784b1f6ace19de1469ce6c1058642dc0d772f9e
TMRGZ <miguelrg5605@outlook.es>
2023-02-02 23:16:30 +0100

    FEATURE: Added missing responses

--- Safebox-Service/src/main/java/com/rviewer/skeletons/application/service/impl/SafeboxApplicationServiceImpl.java
+++ Safebox-Service/src/main/java/com/rviewer/skeletons/application/service/impl/SafeboxApplicationServiceImpl.java
@@ -3,6 +3,9 @@ package com.rviewer.skeletons.application.service.impl;
 import com.rviewer.skeletons.application.model.ItemListDto;
 import com.rviewer.skeletons.application.model.SafeboxDto;
 import com.rviewer.skeletons.application.service.SafeboxApplicationService;
+import com.rviewer.skeletons.domain.exception.SafeboxAlreadyExistsException;
+import com.rviewer.skeletons.domain.exception.SafeboxDoesNotExistException;
+import com.rviewer.skeletons.domain.exception.SafeboxServiceException;
 import com.rviewer.skeletons.domain.model.Item;
 import com.rviewer.skeletons.domain.model.Safebox;
 import com.rviewer.skeletons.domain.service.SafeboxService;
@@ -24,26 +27,60 @@ public class SafeboxApplicationServiceImpl implements SafeboxApplicationService 
     @Override
     @Transactional
     public ResponseEntity<SafeboxDto> createSafebox(String owner) {
-        Safebox newSafebox = safeboxService.createSafebox(owner);
-        SafeboxDto safeboxDto = new SafeboxDto().id(newSafebox.getId().toString());
-        return new ResponseEntity<>(safeboxDto, HttpStatus.CREATED);
+        ResponseEntity<SafeboxDto> response;
+
+        try {
+            Safebox newSafebox = safeboxService.createSafebox(owner);
+            SafeboxDto safeboxDto = new SafeboxDto().id(newSafebox.getId());
+            response = new ResponseEntity<>(safeboxDto, HttpStatus.CREATED);
+
+        } catch (SafeboxAlreadyExistsException e) {
+            response = ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (SafeboxServiceException e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
     }
 
     @Override
     @Transactional
     public ResponseEntity<Void> addItemsToSafebox(String safeboxId, List<String> itemDetailList) {
-        List<Item> itemList = itemDetailList.stream()
-                .map(Item::new)
-                .collect(Collectors.toList());
+        ResponseEntity<Void> response;
 
-        safeboxService.addItemsToSafebox(safeboxId, itemList);
-        return ResponseEntity.ok().build();
+        try {
+            List<Item> itemList = itemDetailList.stream()
+                    .map(Item::new)
+                    .collect(Collectors.toList());
+
+            safeboxService.addItemsToSafebox(safeboxId, itemList);
+
+            response = ResponseEntity.ok().build();
+
+        } catch (SafeboxDoesNotExistException e) {
+            response = ResponseEntity.notFound().build();
+        } catch (SafeboxServiceException e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
     }
 
     @Override
     public ResponseEntity<ItemListDto> getSafeboxItems(String safeboxId) {
-        List<Item> itemList = safeboxService.getSafeboxItems(safeboxId);
-        List<String> itemDetails = itemList.stream().map(Item::getDetail).collect(Collectors.toList());
-        return ResponseEntity.ok().body(new ItemListDto().items(itemDetails));
+        ResponseEntity<ItemListDto> response;
+
+        try {
+            List<Item> itemList = safeboxService.getSafeboxItems(safeboxId);
+            List<String> itemDetails = itemList.stream().map(Item::getDetail).collect(Collectors.toList());
+            response = ResponseEntity.ok().body(new ItemListDto().items(itemDetails));
+
+        } catch (SafeboxDoesNotExistException e) {
+            response = ResponseEntity.notFound().build();
+        } catch (SafeboxServiceException e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
     }
 }
