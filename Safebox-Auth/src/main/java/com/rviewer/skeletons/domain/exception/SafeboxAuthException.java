3b1421aead44a87eb511841b3ca2969f8eef54a3
TMRGZ <miguelrg5605@outlook.es>
2023-02-03 21:16:33 +0100

    FEATURE: Added custom exceptions

---
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/domain/exception/SafeboxAuthException.java
@@ -0,0 +1,7 @@
+package com.rviewer.skeletons.domain.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class SafeboxAuthException extends RuntimeException {
+}
