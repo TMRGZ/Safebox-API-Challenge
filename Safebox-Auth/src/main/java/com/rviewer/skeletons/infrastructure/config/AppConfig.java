6b885cfceacbd69a30e9793ea916c7c204e157ae
TMRGZ <miguelrg5605@outlook.es>
2023-02-04 17:50:07 +0100

    FEATURE: Added maxTries configuration

---
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/infrastructure/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.rviewer.skeletons.infrastructure.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "appconfig")
+public class AppConfig {
+
+    @NotNull
+    private Long maxTries;
+
+}
