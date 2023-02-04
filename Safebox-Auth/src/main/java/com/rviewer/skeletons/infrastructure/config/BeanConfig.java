a96372d1f58a3d2e3f442dff26a844ad66ae08ff
TMRGZ <miguelrg5605@outlook.es>
2023-02-04 17:18:52 +0100

    FEATURE: Added BeanConfig

---
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/infrastructure/config/BeanConfig.java
@@ -0,0 +1,19 @@
+package com.rviewer.skeletons.infrastructure.config;
+
+import com.rviewer.skeletons.domain.repository.UserRepository;
+import com.rviewer.skeletons.domain.service.PasswordService;
+import com.rviewer.skeletons.domain.service.TokenService;
+import com.rviewer.skeletons.domain.service.UserService;
+import com.rviewer.skeletons.domain.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public UserService userService(TokenService tokenService, PasswordService passwordService, UserRepository userRepository) {
+        return new UserServiceImpl(tokenService, passwordService, userRepository);
+    }
+
+}
