2348f08df5e52bdc7709413b3761cc1a26991ba3
TMRGZ <miguelrg5605@outlook.es>
2023-02-04 19:08:44 +0100

    FEATURE: Implemented password service

--- Safebox-Auth/src/main/java/com/rviewer/skeletons/infrastructure/service/impl/PasswordServiceImpl.java
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/infrastructure/service/impl/PasswordServiceImpl.java
@@ -1,17 +1,23 @@
 package com.rviewer.skeletons.infrastructure.service.impl;
 
 import com.rviewer.skeletons.domain.service.PasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PasswordServiceImpl implements PasswordService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public String encodePassword(String plainPassword) {
-        return null;
+        return passwordEncoder.encode(plainPassword);
     }
 
     @Override
     public boolean checkPassword(String encodedPassword, String plainPassword) {
-        return false;
+        return passwordEncoder.matches(plainPassword, encodedPassword);
     }
 }
