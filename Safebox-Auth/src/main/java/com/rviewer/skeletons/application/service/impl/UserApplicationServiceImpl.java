e06c4695e286058e031d3a9b4018acfe54dcc8e9
TMRGZ <miguelrg5605@outlook.es>
2023-02-06 21:26:17 +0100

    FIX: Added missing responses to application layer

--- Safebox-Auth/src/main/java/com/rviewer/skeletons/application/service/impl/UserApplicationServiceImpl.java
+++ Safebox-Auth/src/main/java/com/rviewer/skeletons/application/service/impl/UserApplicationServiceImpl.java
@@ -4,9 +4,12 @@ import com.rviewer.skeletons.application.model.LoginResponseDto;
 import com.rviewer.skeletons.application.model.RegisteredUserDto;
 import com.rviewer.skeletons.application.model.UserDto;
 import com.rviewer.skeletons.application.service.UserApplicationService;
+import com.rviewer.skeletons.domain.exception.UserAlreadyRegisteredException;
+import com.rviewer.skeletons.domain.exception.UserDoesNotExistException;
 import com.rviewer.skeletons.domain.model.user.SafeboxUser;
 import com.rviewer.skeletons.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +23,32 @@ public class UserApplicationServiceImpl implements UserApplicationService {
     @Override
     @Transactional
     public ResponseEntity<RegisteredUserDto> createUser(UserDto request) {
-        SafeboxUser newSafeboxUser = userService.createUser(request.getUsername(), request.getPassword());
-        return ResponseEntity.ok(new RegisteredUserDto().id(newSafeboxUser.getId()));
+        ResponseEntity<RegisteredUserDto> response;
+
+        try {
+            SafeboxUser newSafeboxUser = userService.createUser(request.getUsername(), request.getPassword());
+            response = new ResponseEntity<>(new RegisteredUserDto().id(newSafeboxUser.getId()), HttpStatus.CREATED);
+
+        } catch (UserAlreadyRegisteredException e) {
+            response = new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        return response;
     }
 
     @Override
     @Transactional
     public ResponseEntity<LoginResponseDto> loginUser(String userId) {
-        String token = userService.generateUserToken(userId);
-        return ResponseEntity.ok(new LoginResponseDto().token(token));
+        ResponseEntity<LoginResponseDto> response;
+
+        try {
+            String token = userService.generateUserToken(userId);
+            response = ResponseEntity.ok(new LoginResponseDto().token(token));
+
+        } catch (UserDoesNotExistException e) {
+            response = ResponseEntity.notFound().build();
+        }
+
+        return response;
     }
 }
