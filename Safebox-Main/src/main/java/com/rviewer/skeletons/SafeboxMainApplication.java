867169ec3d7c6bd1f79d2b9cd73c5ba46ecafd94
TMRGZ <miguelrg5605@outlook.es>
2023-02-07 22:50:25 +0100

    FEATURE: Added empty module

---
+++ Safebox-Main/src/main/java/com/rviewer/skeletons/SafeboxMainApplication.java
@@ -0,0 +1,13 @@
+package com.rviewer.skeletons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeboxMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeboxMainApplication.class, args);
+	}
+
+}
---
+++ Safebox-Main/src/test/java/com/rviewer/skeletons/SafeboxMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rviewer.skeletons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeboxMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
