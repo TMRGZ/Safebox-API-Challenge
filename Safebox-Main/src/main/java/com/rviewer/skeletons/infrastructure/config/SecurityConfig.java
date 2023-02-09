e93c1c3b7092cb3ca4bea8cb716e5273c3c8a792
TMRGZ <miguelrg5605@outlook.es>
2023-02-09 22:48:28 +0100

    FEATURE: Added security config

---
+++ Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.rviewer.skeletons.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests()
+                .antMatchers("/safebox").permitAll()
+                .antMatchers("/error").permitAll()
+                .anyRequest().authenticated().and()
+                .httpBasic().and()
+                .csrf().disable()
+                .build();
+    }
+}
