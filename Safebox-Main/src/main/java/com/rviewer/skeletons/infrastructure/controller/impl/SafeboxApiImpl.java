8c8a0c58644b2698d9fd6c9d41b565bf3c247efa
TMRGZ <miguelrg5605@outlook.es>
2023-02-11 18:28:16 +0100

    FEATURE: Make use of SafeboxApplicationService

--- Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/controller/impl/SafeboxApiImpl.java
+++ Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/controller/impl/SafeboxApiImpl.java
@@ -4,30 +4,35 @@ import com.rviewer.skeletons.application.model.CreateSafeboxRequestDto;
 import com.rviewer.skeletons.application.model.CreatedSafeboxDto;
 import com.rviewer.skeletons.application.model.ItemListDto;
 import com.rviewer.skeletons.application.model.SafeboxKeyDto;
+import com.rviewer.skeletons.application.service.SafeboxApplicationService;
 import com.rviewer.skeletons.infrastructure.controller.SafeboxApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SafeboxApiImpl implements SafeboxApi {
 
+    @Autowired
+    private SafeboxApplicationService safeboxApplicationService;
+
     @Override
     public ResponseEntity<ItemListDto> getSafeboxItems(String id) {
-        return SafeboxApi.super.getSafeboxItems(id);
+        return safeboxApplicationService.getSafeboxItems(id);
     }
 
     @Override
     public ResponseEntity<SafeboxKeyDto> openSafebox(String id) {
-        return SafeboxApi.super.openSafebox(id);
+        return safeboxApplicationService.openSafebox(id);
     }
 
     @Override
     public ResponseEntity<CreatedSafeboxDto> postSafebox(CreateSafeboxRequestDto createSafeboxRequestDto) {
-        return SafeboxApi.super.postSafebox(createSafeboxRequestDto);
+        return safeboxApplicationService.createSafebox(createSafeboxRequestDto);
     }
 
     @Override
     public ResponseEntity<Void> putSafeboxItems(String id, ItemListDto itemListDto) {
-        return SafeboxApi.super.putSafeboxItems(id, itemListDto);
+        return safeboxApplicationService.addItemsToSafebox(id, itemListDto);
     }
 }
