9b21bc9cc0902b19649466771fb3d868f2f88424
TMRGZ <miguelrg5605@outlook.es>
2023-02-14 22:06:47 +0100

    FEATURE: Enabled providers

--- Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
+++ Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.rviewer.skeletons.infrastructure.config;
 
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import com.rviewer.skeletons.infrastructure.provider.impl.BasicAuthProviderImpl;
+import com.rviewer.skeletons.infrastructure.provider.impl.JwtProviderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, JwtProviderImpl jwtProvider, BasicAuthProviderImpl basicAuthProvider) throws Exception {
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        return builder.authenticationProvider(basicAuthProvider)
+                .authenticationProvider(jwtProvider)
+                .build();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests()
+        httpSecurity.authorizeHttpRequests()
                 .antMatchers("/safebox").permitAll()
                 .antMatchers("/error").permitAll()
                 .anyRequest().authenticated().and()
                 .httpBasic().and()
                 .csrf().disable()
-                .build();
+                .oauth2ResourceServer().jwt();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return new NimbusJwtDecoder(new DefaultJWTProcessor<>());
     }
 }
