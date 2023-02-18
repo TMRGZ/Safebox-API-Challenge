fe9915f69a2b0f74217beaf8b9c3298eb2b79051
TMRGZ <miguelrg5605@outlook.es>
2023-02-18 01:13:57 +0100

    FEATURE: Added JWT and Basic auth methods depending on the endpoint

--- Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
+++ Safebox-Main/src/main/java/com/rviewer/skeletons/infrastructure/config/SecurityConfig.java
@@ -5,9 +5,12 @@ import com.rviewer.skeletons.infrastructure.provider.impl.BasicAuthProviderImpl;
 import com.rviewer.skeletons.infrastructure.provider.impl.JwtProviderImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,20 +28,38 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests()
-                .antMatchers("/safebox").permitAll()
-                .antMatchers("/error").permitAll()
-                .anyRequest().authenticated().and()
-                .httpBasic().and()
-                .csrf().disable()
-                .oauth2ResourceServer().jwt();
+    public JwtDecoder jwtDecoder() {
+        return new NimbusJwtDecoder(new DefaultJWTProcessor<>());
+    }
+
+    @Bean
+    public SecurityFilterChain basicFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .regexMatchers(HttpMethod.GET, "/safebox/.+/open").authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
 
         return httpSecurity.build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return new NimbusJwtDecoder(new DefaultJWTProcessor<>());
+    @Order(1)
+    public SecurityFilterChain jwtFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.regexMatcher("/safebox/.+/items")
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return httpSecurity.build();
     }
 }
